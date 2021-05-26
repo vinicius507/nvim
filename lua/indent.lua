952ad309ab8d99c715d3e99ed2b5ce5f7cb998ac
Vinícius Gonçalves de Oliveira <viniciusp.olivera@gmail.com>
2021-05-26 16:09:53 -0300

    add: indent blankline

---
+++ lua/indent.lua
@@ -0,0 +1,18 @@
+local vimg = vim.g
+
+local options = {
+	use_treesitter = true,
+	show_current_context = true,
+	context_patterns = { 'if', 'while', 'for', 'else', 'class', 'function', 'method', 'local' },
+	filetype_exclude = { 'NvimTree' },
+	show_end_of_line = true,
+	char = '│'
+}
+
+local set_options = function(opt)
+	for key, value in pairs(opt) do
+		vimg['indent_blankline_' .. key] = value
+	end
+end
+
+set_options(options)
