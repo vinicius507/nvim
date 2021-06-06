35933c048f8364279695b029181053de4fead320
Vinícius Gonçalves de Oliveira <viniciusp.olivera@gmail.com>
2021-06-06 08:00:29 -0300

    fix: lsp signature

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -42,11 +42,15 @@ end
 setup_servers()
 lsp_signature.on_attach({
 	bind = true,
-	doc_lines = 10,
+	doc_lines = 2,
+	floating_window = true,
 	hint_enable = false,
 	hint_prefix = "",
 	hint_scheme = "String",
 	use_lspsaga = false,
+	hi_parameter = "Search",
+	max_height = 12,
+	max_width = 120,
 	handler_opts = {
 		border = "single"
 	},
