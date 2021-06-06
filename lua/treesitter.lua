a672ae66b5734a9457df4cf384e107210f8c72f0
Vinícius Gonçalves de Oliveira <viniciusp.olivera@gmail.com>
2021-06-06 17:46:51 -0300

    add: css in treesitter

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-	ensure_installed = {'c', 'lua', 'javascript', 'typescript', 'bash', 'python'},
+	ensure_installed = {'c', 'lua', 'javascript', 'typescript', 'bash', 'python', 'css'},
 	highlight = {
 		enable = true,
 	},
