f05380485014f1840678ee07fccb4c6e1992732d
Vinicius Gonçalves de Oliveira <vinicius@myriaddev.me>
2021-07-10 16:57:59 -0300

    fix: forgot org config file

---
+++ lua/org.lua
@@ -0,0 +1,8 @@
+require('orgmode').setup({
+	org_agenda_files = { '~/Documents/org/**/*' },
+	org_default_notes_file = '~/Documents/org/notes.org',
+})
+
+require('org-bullets').setup {
+	symbols = { '◉', '○', '✸', '✿' }
+}
