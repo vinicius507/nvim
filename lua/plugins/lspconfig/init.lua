d9a50c3895cd9c02d59257f5f283d92eebc93f2a
Vinicius Gonçalves de Oliveira <vinicius@myriaddev.me>
2021-08-11 18:42:39 -0300

    feat: modified prefix in lsp virtual text

--- lua/plugins/lspconfig/init.lua
+++ lua/plugins/lspconfig/init.lua
@@ -20,7 +20,10 @@ setup_servers()
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
 	vim.lsp.diagnostic.on_publish_diagnostics,
 	{
-		virtual_text = true,
+		virtual_text = {
+			spacing = 4,
+			prefix = '●',
+		},
 		signs = true,
 		underline = true,
 		update_in_insert = true,
