643c256175c21a98acdb89f2809fba3c1b0f8b7a
Vinicius Gonçalves de Oliveira <vinicius@myriaddev.me>
2021-08-16 09:16:01 -0300

    feat: neogit now sets ssh-agent environment variables

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -17,3 +17,23 @@ neogit.setup({
 		diffview = true,
 	},
 })
+
+local job
+ok, job = pcall(require, 'plenary.job')
+
+local result = job
+	:new({
+		command = 'ssh-agent',
+		args = { '-s' },
+	})
+	:sync()
+
+local env = vim.env
+if result and #result then
+	for _, v in ipairs(result) do
+		local key, value = v:match('^(.*)=(.*);%s+export.*$')
+		if key and value then
+			env[key] = value
+		end
+	end
+end
