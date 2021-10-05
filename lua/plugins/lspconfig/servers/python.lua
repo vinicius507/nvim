0b4404ca492060d2cd7ea66246e68a6ecd716e85
Vinicius Gonçalves de Oliveira <vinicius@myriaddev.me>
2021-10-05 10:28:39 -0300

    fix: python path

--- lua/plugins/lspconfig/servers/python.lua
+++ lua/plugins/lspconfig/servers/python.lua
@@ -3,8 +3,10 @@ local M = {}
 M.config = {
 	settings = {
 		python = {
-			pythonPath = string.format('%s/python', vim.env.ASDF_USER_SHIMS)
-				or 'python',
+			pythonPath = vim.env.ASDF_USER_SHIMS and string.format(
+				'%s/python',
+				vim.env.ASDF_USER_SHIMS
+			) or 'python',
 			venvPath = string.format('%s/.local/virtualenvs', os.getenv('HOME')),
 			analysis = {
 				diagnosticMode = 'workspace',
