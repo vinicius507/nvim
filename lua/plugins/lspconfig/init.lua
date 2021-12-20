51f98bbdf7e145cef5b3518ace9b403a76dcb47b
Vinicius Gonçalves de Oliveira <vinicius@myriaddev.me>
2021-12-20 13:54:41 -0300

    fix(lspconfig): removed old diagnostics format

--- lua/plugins/lspconfig/init.lua
+++ lua/plugins/lspconfig/init.lua
@@ -23,16 +23,3 @@ lspinstall.on_server_ready(function(server)
 	)
 	server:setup(opts)
 end)
-
--- vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
--- 	vim.lsp.diagnostic.on_publish_diagnostics,
--- 	{
--- 		virtual_text = {
--- 			spacing = 4,
--- 			prefix = '●',
--- 		},
--- 		signs = true,
--- 		underline = true,
--- 		update_in_insert = true,
--- 	}
--- )
