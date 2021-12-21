941d1ce822b2c95c6f8391a3a231e269357c8bef
Vinicius Gonçalves de Oliveira <vinicius@myriaddev.me>
2021-12-20 21:50:18 -0300

    fix(telescope): removed unnecessary stuff

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,15 +8,9 @@ local ok2, trouble = pcall(require, 'trouble.providers.telescope')
 
 telescope.setup({
 	defaults = {
-		file_sorter = require('telescope.sorters').get_fzy_sorter,
 		prompt_prefix = '  ',
 		selection_caret = ' ',
 		color_devicons = true,
-
-		file_previewer = require('telescope.previewers').vim_buffer_cat.new,
-		grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
-		qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
-
 		mappings = {
 			i = {
 				['<C-x>'] = false,
