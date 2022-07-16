e6088640fd67a22d4a6db3c1f77686835c7548e3
Vini <vini@42sp.org.br>
2022-07-16 03:10:11 -0300

    Adds Catppuccin colorscheme

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,6 +3,14 @@ local packer = require("packer")
 packer.startup({
 	function()
 		packer.use("wbthomason/packer.nvim")
+		packer.use({
+			"catppuccin/nvim",
+			as = "catppuccin",
+			event = "VimEnter",
+			config = function()
+				require("plugins.catppuccin")
+			end,
+		})
 	end,
 	config = {},
 })
---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,21 @@
+require("catppuccin").setup({
+	term_colors = true,
+	styles = {
+		comments = "italic",
+		conditionals = "italic",
+		loops = "NONE",
+		functions = "NONE",
+		keywords = "italic",
+		strings = "NONE",
+		variables = "NONE",
+		numbers = "NONE",
+		booleans = "NONE",
+		properties = "NONE",
+		types = "NONE",
+		operators = "NONE",
+	},
+	integrations = {},
+})
+
+vim.g.flavor = "mocha"
+vim.cmd([[colorscheme catppuccin]])
