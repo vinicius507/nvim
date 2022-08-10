7141cc412c6d8f1e78cebb545d71100e1dd8ed4e
Vinicius Oliveira <vinicius@myriad.codes>
2022-08-10 19:17:00 -0300

    Updates toggleterm config

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,7 +3,7 @@ local Terminal = require("toggleterm.terminal").Terminal
 
 require("toggleterm").setup({
 	size = 15,
-	open_mapping = nil,
+	open_mapping = [[<C-\>]],
 	hide_numbers = true,
 	shade_terminals = true,
 	start_in_insert = true,
@@ -17,19 +17,21 @@ require("toggleterm").setup({
 	winbar = { enabled = true },
 })
 
-local shell = Terminal:new()
 local runner = Terminal:new({ close_on_exit = false })
-mappings.add({
-	"<Leader>ot",
-	function()
-		shell:toggle()
-	end,
-	description = "Open terminal",
-})
+
+local function get_compile_cmd()
+	local ok, last_cmd = pcall(vim.api.nvim_tabpage_get_var, 0, "last_compile_cmd")
+	if not ok then
+		last_cmd = "make -C ."
+	end
+	local compile_cmd = vim.fn.input("Compile command: ", last_cmd)
+	vim.api.nvim_tabpage_set_var(0, "last_compile_cmd", compile_cmd)
+end
+
 mappings.add({
 	"<Leader>cc",
 	function()
-		runner.cmd = vim.fn.input("Compile command: ", "make -C .")
+		runner.cmd = get_compile_cmd()
 		if runner.cmd ~= "" then
 			runner:toggle()
 		end
