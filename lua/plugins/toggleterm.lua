b11838a33b1b4e5e427cfea88e05f55c8d6580ab
Vinicius Oliveira <vinicius@myriad.codes>
2022-08-11 16:01:09 -0300

    Fixes compile command terminal

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -26,6 +26,7 @@ local function get_compile_cmd()
 	end
 	local compile_cmd = vim.fn.input("Compile command: ", last_cmd)
 	vim.api.nvim_tabpage_set_var(0, "last_compile_cmd", compile_cmd)
+	return compile_cmd
 end
 
 mappings.add({
