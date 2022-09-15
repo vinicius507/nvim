45ba7a36f70640fb4b9747fa9ae6d103ddd8f83e
Vinicius Oliveira <vinicius@myriad.codes>
2022-09-14 23:07:48 -0300

    Adds mapping for sourcing vim/lua file

---
+++ ftplugin/lua.lua
@@ -0,0 +1,16 @@
+local mappings = require("mappings")
+
+mappings.add({
+	"<Leader>fS",
+	function()
+		local ft = vim.api.nvim_buf_get_option(0, "filetype")
+		if ft ~= "lua" and ft ~= "vim" then
+			return
+		end
+		if vim.fn.confirm(string.format("Source %s?", vim.fn.expand("%")), "&Yes\n&No", 1) then
+			vim.cmd([[source]])
+		end
+	end,
+	buffer = vim.api.nvim_get_current_buf(),
+	description = "Source file",
+})
