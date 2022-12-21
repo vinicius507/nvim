c783e63fe755651fc6f4de68167e5457d8d88874
Vinicius Oliveira <vinicius@myriad.codes>
2022-12-21 10:54:36 -0300

    Fixes whichkey.nvim config

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,55 +1,55 @@
 local wk = require("which-key")
 
 wk.setup({
-	{
-		plugins = {
-			marks = true,
-			registers = true,
-			spelling = {
-				enabled = true,
-				suggestions = 20,
-			},
-			presets = {
-				operators = true,
-				motions = true,
-				text_objects = true,
-				windows = true,
-				nav = true,
-				z = true,
-				g = true,
-			},
+	plugins = {
+		marks = true,
+		registers = true,
+		spelling = {
+			enabled = true,
+			suggestions = 20,
 		},
-		operators = { gc = "Comments" },
-		key_labels = {
-			["<leader>"] = "LEADER",
+		presets = {
+			operators = true,
+			motions = true,
+			text_objects = true,
+			windows = true,
+			nav = true,
+			z = true,
+			g = true,
 		},
-		icons = {
-			breadcrumb = "»",
-			separator = "➜",
-			group = "+",
-		},
-		popup_mappings = {
-			scroll_down = "<c-d>",
-			scroll_up = "<c-u>",
-		},
-		window = {
-			border = "single",
-			position = "bottom",
-			margin = { 1, 0, 1, 0 },
-			padding = { 2, 2, 2, 2 },
-			winblend = 0,
-		},
-		layout = {
-			height = { min = 4, max = 25 },
-			width = { min = 20, max = 50 },
-			spacing = 3,
-			align = "left",
-		},
-		ignore_missing = false,
-		hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " },
-		show_help = true,
 	},
+	operators = { gc = "Comments" },
+	key_labels = {
+		["<leader>"] = "SPC",
+	},
+	icons = {
+		breadcrumb = "»",
+		separator = "➜",
+		group = "+",
+	},
+	popup_mappings = {
+		scroll_down = "<c-d>",
+		scroll_up = "<c-u>",
+	},
+	window = {
+		border = "single",
+		position = "bottom",
+		margin = { 1, 0, 1, 0 },
+		padding = { 2, 2, 2, 2 },
+		winblend = 0,
+	},
+	layout = {
+		height = { min = 4, max = 25 },
+		width = { min = 20, max = 50 },
+		spacing = 3,
+		align = "left",
+	},
+	ignore_missing = false,
+	hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " },
+	show_help = true,
+	show_keys = false,
 })
+
 wk.register({
 	["<Leader>"] = {
 		["1"] = "which_key_ignore",
@@ -65,9 +65,10 @@ wk.register({
 		c = { name = "Code" },
 		f = { name = "Telescope" },
 		g = { name = "Git", t = "Toggle" },
-		o = { name = "Open" },
+		h = { name = "Hop" },
 		p = { name = "Packer" },
+		t = { name = "Todo" },
 		w = { name = "Window" },
-		z = { name = "Zen mode" },
+		z = { name = "Zen" },
 	},
 })
