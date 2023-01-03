d00a5bf63229958bdb6cd8ee538c093be3f17c0a
Vinicius Oliveira <vinicius@myriad.codes>
2023-01-02 22:04:50 -0300

    feat: ✨ catppuccin

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,36 @@
+return {
+	"catppuccin/nvim",
+	build = ":CatppuccinCompile",
+	config = function()
+		require("catppuccin").setup({
+			flavour = "mocha",
+			term_colors = true,
+			styles = {
+				comments = { "italic" },
+				conditionals = { "italic" },
+				keywords = { "italic" },
+			},
+			integrations = {
+				cmp = true,
+				treesitter = true,
+				telescope = true,
+				markdown = true,
+				noice = true,
+				notify = true,
+				neogit = true,
+				barbar = true,
+				gitsigns = true,
+				hop = true,
+				nvimtree = {
+					enabled = true,
+					show_root = true,
+					transparent_panel = false,
+				},
+			},
+		})
+
+		vim.cmd.colorscheme("catppuccin")
+	end,
+	lazy = false,
+	priority = 1000,
+}
