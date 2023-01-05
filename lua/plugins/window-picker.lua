804f953d3d01e0ae9d291d80f85442dfd6eba543
Vini <vini@42sp.org.br>
2023-01-05 12:08:13 -0300

    feat: window-picker

---
+++ lua/plugins/window-picker.lua
@@ -0,0 +1,33 @@
+local remap = require("myriad.keymaps").remap
+
+return {
+	"s1n7ax/nvim-window-picker",
+	tag = "v1.*",
+	config = function()
+		local wp = require("window-picker")
+		local colors = require("catppuccin.palettes").get_palette()
+
+		wp.setup({
+			autoselect_one = true,
+			include_current_win = true,
+			filter_rules = {
+				bo = {
+					filetype = {
+						"neo-tree",
+						"neo-tree-popup",
+						"notify",
+					},
+					buftype = {
+						"quickfix",
+						"terminal",
+					},
+				},
+			},
+			fg_color = colors.base,
+			current_win_hl_color = colors.red,
+			other_win_hl_color = colors.green,
+		})
+
+		remap("<Leader>wp", wp.pick_window, { desc = "Pick window" })
+	end,
+}
