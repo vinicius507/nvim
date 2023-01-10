c16683236007cf3f225f75f8a6e082b3b21fcc5a
Vinicius Oliveira <vini@42sp.myriad.codes.beta.tailscale.net>
2023-01-10 16:47:13 -0300

    fix: hop mappings

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -2,39 +2,11 @@ return {
 	"phaazon/hop.nvim",
 	keys = {
 		{
-			"<Leader>hc",
-			function()
-				require("hop").hint_char1()
-			end,
-			desc = "Hop to char",
-		},
-		{
-			"<Leader>hb",
+			"gs<Space>",
 			function()
 				require("hop").hint_char2()
 			end,
 			desc = "Hop to bigram",
-		},
-		{
-			"<Leader>hl",
-			function()
-				require("hop").hint_lines_skip_whitespace()
-			end,
-			desc = "Hop to line",
-		},
-		{
-			"<Leader>hp",
-			function()
-				require("hop").hint_patterns()
-			end,
-			desc = "Hop to pattern",
-		},
-		{
-			"<Leader>hw",
-			function()
-				require("hop").hint_words()
-			end,
-			desc = "Hop to word",
 		},
 	},
 	opts = {
