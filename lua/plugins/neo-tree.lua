2da1de7b732221dfc291c9d8de70ef473aa39973
Vinicius Oliveira <vini@42sp.myriad.codes.beta.tailscale.net>
2023-01-11 11:01:58 -0300

    feat: neo-tree configuration

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,11 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	opts = {
+		sort_case_insensitive = true,
+		window = {
+			width = 32,
+		},
+		hijack_netrw_behavior = "open_default",
+		use_libuv_file_watcher = true,
+	},
+}
