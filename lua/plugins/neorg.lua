790042d8b7c397de1535dec6d92b684cb2986736
Vini <vini@42sp.org.br>
2023-01-17 13:05:26 -0300

    feat: neorg keys

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -3,32 +3,56 @@ return {
 	ft = "norg",
 	cmd = "Neorg",
 	build = ":Neorg sync-parsers",
-	opts = {
-		load = {
-			["core.defaults"] = {},
-			["core.export"] = {},
-			["core.presenter"] = {
-				config = {
-					zen_mode = "zen-mode",
-				},
+	keys = function(plugin)
+		return {
+			{ "<Leader>n", desc = "Neorg" },
+			{
+				"<Leader>nj",
+				function()
+					vim.cmd.Neorg("journal")
+				end,
+				desc = "Journal",
 			},
-			["core.norg.concealer"] = {},
-			["core.norg.completion"] = {
-				config = {
-					engine = "nvim-cmp",
-				},
+			{
+				"<Leader>nw",
+				function()
+					vim.ui.select(vim.tbl_keys(plugin.workspaces), {
+						prompt = "Switch to Neorg workspace: ",
+					}, function(workspace)
+						vim.cmd.Neorg("workspace", workspace)
+					end)
+				end,
+				desc = "Switch to workspace",
 			},
-			["core.norg.dirman"] = {
-				config = {
-					workspaces = {
-						work = "~/Documents/work/",
-						personal = "~/Documents/personal/",
+		}
+	end,
+	workspaces = {
+		work = "~/Documents/work/",
+		personal = "~/Documents/personal/",
+	},
+	opts = function(plugin)
+		return {
+			load = {
+				["core.defaults"] = {},
+				["core.export"] = {},
+				["core.presenter"] = {
+					config = {
+						zen_mode = "zen-mode",
 					},
 				},
+				["core.norg.concealer"] = {},
+				["core.norg.completion"] = {
+					config = { engine = "nvim-cmp" },
+				},
+				["core.norg.dirman"] = {
+					config = {
+						workspaces = plugin.workspaces,
+					},
+				},
+				["core.integrations.nvim-cmp"] = {},
 			},
-			["core.integrations.nvim-cmp"] = {},
-		},
-	},
+		}
+	end,
 	dependencies = {
 		"folke/zen-mode.nvim",
 		"nvim-lua/plenary.nvim",
