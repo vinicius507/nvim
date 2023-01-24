84e9ac15da780a105cca8e7c833c41c38c5aae6a
Vini <vini@42sp.org.br>
2023-01-24 13:07:23 -0300

    fix: there's no need to overwrite opts

--- lua/plugins/window-picker.lua
+++ lua/plugins/window-picker.lua
@@ -10,29 +10,31 @@ return {
 			desc = "Pick window",
 		},
 	},
-	opts = function(_, opts)
+	opts = function()
 		local colors = require("catppuccin.palettes").get_palette()
 
-		opts.autoselect_one = true
-		opts.include_current_win = true
-		opts.filter_rules = {
-			bo = {
-				filetype = {
-					"neo-tree",
-					"neo-tree-popup",
-					"notify",
-				},
-				buftype = {
-					"quickfix",
-					"terminal",
+		return {
+			autoselect_one = true,
+			include_current_win = true,
+			filter_rules = {
+				bo = {
+					filetype = {
+						"neo-tree",
+						"neo-tree-popup",
+						"notify",
+					},
+					buftype = {
+						"quickfix",
+						"terminal",
+					},
 				},
 			},
+			fg_color = colors.base,
+			current_win_hl_color = colors.red,
+			other_win_hl_color = colors.green,
+			selection_display = function(char)
+				return string.format("[%s] %s", char, "%f")
+			end,
 		}
-		opts.fg_color = colors.base
-		opts.current_win_hl_color = colors.red
-		opts.other_win_hl_color = colors.green
-		opts.selection_display = function(char)
-			return string.format("[%s] %s", char, "%f")
-		end
 	end,
 }
