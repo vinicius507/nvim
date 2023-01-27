842013bdc9badeb6444b663c010473358bd40148
Vini <vini@42sp.org.br>
2023-01-27 11:43:31 -0300

    feat: vim-matchup

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -13,4 +13,11 @@ return {
 			},
 		},
 	},
+	{
+		"andymass/vim-matchup",
+		event = "BufReadPost",
+		config = function()
+			vim.g.matchup_matchparen_offscreen = { method = "status_manual" }
+		end,
+	},
 }
