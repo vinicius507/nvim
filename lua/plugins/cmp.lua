9200c2664dd23870a2f78e5a2d62eb465d820877
Vini <vini@42sp.org.br>
2023-01-27 10:10:12 -0300

    feat: neogen

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,11 +3,26 @@ return {
 	---@param opts cmp.ConfigSchema
 	opts = function(_, opts)
 		local cmp = require("cmp")
+		local neogen = require("neogen")
 
 		opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
 			{ name = "emoji" },
 			{ name = "neorg" },
 		}, 0, #opts.sources))
+		opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
+			if neogen.jumpable() then
+				neogen.jump_next()
+			else
+				fallback()
+			end
+		end)
+		opts.mapping["<S-Tab>"] = cmp.mapping(function(fallback)
+			if neogen.jumpable() then
+				neogen.jump_prev()
+			else
+				fallback()
+			end
+		end)
 		opts.formatting.fields = { "abbr", "kind" }
 		opts.formatting.format = function(entry, vim_item)
 			local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
---
+++ lua/plugins/coding.lua
@@ -0,0 +1,9 @@
+return {
+	{
+		"danymat/neogen",
+		opts = {
+			snippet_engine = "luasnip",
+		},
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+	},
+}
