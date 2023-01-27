aacd32c8e6d35144194a0cf65eee5a2c1c1064f1
Vini <vini@42sp.org.br>
2023-01-27 11:17:04 -0300

    fix: navic background color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,53 +2,58 @@ return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	build = ":CatppuccinCompile",
-	opts = {
-		flavour = "mocha",
-		term_colors = true,
-		styles = {
-			comments = { "italic" },
-			conditionals = { "italic" },
-			keywords = { "italic" },
-		},
-		integrations = {
-			cmp = true,
-			dashboard = true,
-			gitsigns = true,
-			hop = true,
-			illuminate = true,
-			indent_blankline = {
-				enabled = true,
-				colored_indent_levels = true,
+	opts = function()
+		local colors = require("catppuccin.palettes").get_palette()
+
+		return {
+			flavour = "mocha",
+			term_colors = true,
+			styles = {
+				comments = { "italic" },
+				conditionals = { "italic" },
+				keywords = { "italic" },
 			},
-			lsp_trouble = true,
-			markdown = true,
-			native_lsp = {
-				enabled = true,
-				virtual_text = {
-					errors = { "italic" },
-					hints = { "italic" },
-					warnings = { "italic" },
-					information = { "italic" },
+			integrations = {
+				cmp = true,
+				dashboard = true,
+				gitsigns = true,
+				hop = true,
+				illuminate = true,
+				indent_blankline = {
+					enabled = true,
+					colored_indent_levels = true,
 				},
-				underlines = {
-					errors = { "underline" },
-					hints = { "underline" },
-					warnings = { "underline" },
-					information = { "underline" },
+				lsp_trouble = true,
+				markdown = true,
+				native_lsp = {
+					enabled = true,
+					virtual_text = {
+						errors = { "italic" },
+						hints = { "italic" },
+						warnings = { "italic" },
+						information = { "italic" },
+					},
+					underlines = {
+						errors = { "underline" },
+						hints = { "underline" },
+						warnings = { "underline" },
+						information = { "underline" },
+					},
 				},
+				navic = {
+					enabled = true,
+					custom_bg = colors.mantle,
+				},
+				neogit = true,
+				neotree = true,
+				noice = true,
+				notify = true,
+				telescope = true,
+				treesitter = true,
+				which_key = true,
 			},
-			navic = {
-				enabled = true,
-			},
-			neogit = true,
-			neotree = true,
-			noice = true,
-			notify = true,
-			telescope = true,
-			treesitter = true,
-			which_key = true,
-		},
-	},
+		}
+	end,
 	lazy = false,
 	priority = 1000,
 }
