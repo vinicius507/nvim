edf61fed6836e88948ddff65f8355b341c4e6420
Vini <vini@42sp.org.br>
2023-03-05 12:18:10 -0300

    feat(plugins): removes neorg

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -18,67 +18,6 @@ return {
 		dependencies = { "folke/twilight.nvim" },
 	},
 	{
-		"nvim-neorg/neorg",
-		ft = "norg",
-		cmd = "Neorg",
-		build = ":Neorg sync-parsers",
-		keys = function(plugin)
-			return {
-				{ "<Leader>n", desc = "Neorg" },
-				{
-					"<Leader>nj",
-					function()
-						vim.cmd.Neorg("journal")
-					end,
-					desc = "Journal",
-				},
-				{
-					"<Leader>nw",
-					function()
-						vim.ui.select(vim.tbl_keys(plugin.workspaces), {
-							prompt = "Switch to Neorg workspace: ",
-						}, function(workspace)
-							vim.cmd.Neorg("workspace", workspace)
-						end)
-					end,
-					desc = "Switch to workspace",
-				},
-			}
-		end,
-		workspaces = {
-			work = "~/Documents/work/",
-			personal = "~/Documents/personal/",
-		},
-		opts = function(plugin)
-			return {
-				load = {
-					["core.defaults"] = {},
-					["core.export"] = {},
-					["core.presenter"] = {
-						config = {
-							zen_mode = "zen-mode",
-						},
-					},
-					["core.norg.concealer"] = {},
-					["core.norg.completion"] = {
-						config = { engine = "nvim-cmp" },
-					},
-					["core.norg.dirman"] = {
-						config = {
-							workspaces = plugin.workspaces,
-						},
-					},
-					["core.integrations.nvim-cmp"] = {},
-				},
-			}
-		end,
-		dependencies = {
-			"folke/zen-mode.nvim",
-			"nvim-lua/plenary.nvim",
-			"nvim-treesitter/nvim-treesitter",
-		},
-	},
-	{
 		"sindrets/diffview.nvim",
 		keys = {
 			{
@@ -169,8 +108,8 @@ return {
 			},
 			{
 				"<Leader>oy",
-				vim.cmd.ObsidianYesterday,
 				desc = "Open yesterday's note",
+				vim.cmd.ObsidianYesterday,
 			},
 			{
 				"<Leader>op",
