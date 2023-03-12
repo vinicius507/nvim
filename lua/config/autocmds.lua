de3d2ade2373550103028c6e5c65778a66da8fad
Vini <vini@42sp.org.br>
2023-03-12 13:10:35 -0300

    fix: typo

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -14,11 +14,11 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 		})
 		vim.keymap.set("x", "<Leader>ol", vim.cmd.ObsidianLink, {
 			buffer = event.buf,
-			desc = "Linke to note",
+			desc = "Link to note",
 		})
 		vim.keymap.set("x", "<Leader>oL", vim.cmd.ObsidianLinkNew, {
 			buffer = event.buf,
-			desc = "Linke to new note",
+			desc = "Link to new note",
 		})
 		vim.keymap.set("n", "<Leader>oT", vim.cmd.ObsidianTemplate, {
 			buffer = event.buf,
