cd58c92c9c4a24280ad78bcd85c430055faffb2e
Vini <vini@42sp.org.br>
2023-04-13 10:25:35 -0300

    hotfix(autocmds): handles cpp files in autoformat

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -42,7 +42,15 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 		"c",
 		"cpp",
 	},
-	callback = function(_)
+	callback = function(event)
+		local enabled_extensions = { "cpp", "hpp" }
+		local file_ext = vim.fn.fnamemodify(event.file, ":e")
+
+		-- Since vim sets .h files filetype to cpp, we need to handle .cpp and .hpp files.
+		if vim.tbl_contains(enabled_extensions, file_ext) then
+			return
+		end
+
 		vim.b.autoformat = false
 	end,
 })
