bbc39eccb0ae682b354876474048578cef4f7009
Vinicius Oliveira <vinicius@myriad.codes>
2023-05-10 00:12:41 -0300

    feat: removes incline.nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -6,33 +6,6 @@ return {
 		end,
 	},
 	{
-		"b0o/incline.nvim",
-		event = "BufReadPre",
-		opts = function()
-			local colors = require("catppuccin.palettes").get_palette()
-
-			return {
-				highlight = {
-					groups = {
-						InclineNormal = { guifg = colors.text, guibg = colors.mantle },
-						InclineNormalNC = { guifg = colors.overlay0, guibg = colors.mantle },
-					},
-				},
-				window = {
-					margin = {
-						vertical = 0,
-						horizontal = 1,
-					},
-				},
-				render = function(props)
-					local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-					local icon, color = require("nvim-web-devicons").get_icon_color(filename)
-					return { { icon, guifg = color }, { " " }, { filename } }
-				end,
-			}
-		end,
-	},
-	{
 		"anuvyklack/windows.nvim",
 		event = "WinNew",
 		keys = {
