5d7280fde41ff019f34520be1f525e1ff93c4803
Vinicius Oliveira <vinicius@myriad.codes>
2023-06-26 18:19:09 -0300

    fix(noice.nvim): use default routes and filters

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -50,25 +50,29 @@ return {
 	},
 	{
 		"folke/noice.nvim",
-		opts = {
-			presets = { lsp_doc_border = true },
-			routes = {
+		opts = function(_, opts)
+			opts.presets.lsp_doc_border = true
+			opts.routes = vim.list_extend(opts.routes, {
 				{
 					filter = {
 						cmdline = true,
 						event = "msg_show",
+						cleared = true,
+						["not"] = {
+							kind = "",
+						},
 					},
 					view = "cmdline_output",
 				},
-			},
-			views = { cmdline_output = { enter = true } },
-			commands = {
+			})
+			opts.views = { cmdline_output = { enter = true } }
+			opts.commands = {
 				all = {
 					view = "split",
 					opts = { enter = true, format = "details" },
 					filter = {},
 				},
-			},
-		},
+			}
+		end,
 	},
 }
